c83b70581283bd2d508ed4c64ce95974cf4d5ef7
Ross <rossmorran@hotmail.co.uk>
2025-04-23 23:56:52 +0100

    Add api_call_log_id to the disruption table

--- get_api_data.py
+++ get_api_data.py
@@ -12,8 +12,10 @@ class TFLData:
     """A class containing methods to get data from the TFL API
     and to write that data to a Snowflake database"""
     
-    def log_data(self, timestamp: datetime.datetime, http_code: int, error_text: str | None, disruption_count: int | None):
-        """Logs metadata about the API call to a database table"""
+    def log_data(self, timestamp: datetime.datetime, http_code: int, error_text: str | None, disruption_count: int | None) -> int | None:
+        """Logs metadata about the API call to the api_call_log table.
+        If data is successfully inserted, returns the id of the new record,
+        otherwise returns None."""
         
         conn = self.get_connection()
         # need to protect against SQL injection?
@@ -30,12 +32,29 @@ class TFLData:
                 VALUES('{timestamp}', {http_code}, {error_text}, {disruption_count})"""
             )
         except Exception as e:
-            print(f'Error when trying to insert data into the db: {e}')
+            conn.close()
+            # is it right to raise an exception here - wouldn't we still want the disruption data?
+            raise Exception(f'Error when trying to insert log data into the db: {e}')
+
+        try:
+            api_call_log_id: int = conn.cursor().execute(
+                f"""SELECT api_call_log_id
+                FROM api_call_log
+                ORDER BY timestamp DESC
+                LIMIT 1"""
+            ).fetchone()[0]  
+        except Exception as e:
+            conn.close()
+            # is it right to raise an exception here - wouldn't we still want the disruption data?
+            raise Exception(f'Error when trying to get the api_call_log_id: {e}')
         
         conn.close()
         
+        return api_call_log_id if api_call_log_id else None
+        
     def get_api_data(self) -> tuple[list[dict], datetime.datetime]:
-        """Fetches data from the API"""
+        """Attempts to fetches disruption data from the API then
+        calls another method which logs metadata about the API call."""
 
         modes: str = "tube, dlr"
         url: str = f"https://api.tfl.gov.uk/Line/Mode/{modes}/Disruption"
@@ -48,8 +67,8 @@ class TFLData:
 
             if response.status_code == 200:
                 data: list[dict] = response.json()
-                self.log_data(timestamp, response.status_code, None, len(data))
-                return data, timestamp
+                api_call_log_id = self.log_data(timestamp, response.status_code, None, len(data))
+                return data, timestamp, api_call_log_id
 
             # log error if call unsuccessful
             self.log_data(timestamp, response.status_code, response.text, None)
@@ -77,20 +96,20 @@ class TFLData:
         except Exception as e:
             raise Exception(f"Error when trying to make db connection: {e}")
 
-    def write_data(self, data, timestamp) -> None:
-        """Writes data to a Snowflake database"""
-
+    def write_data(self, data, timestamp: datetime.datetime, api_call_log_id: int) -> None:
+        """Inserts disruption data into the disruption table."""
+        
         conn = self.get_connection()
 
         for msg in data:
             msg_json = json.dumps(msg)
             try:
                 conn.cursor().execute(
-                    f"""INSERT INTO disruption(response, time_received) 
-                    SELECT PARSE_JSON('{msg_json}'), '{timestamp}';"""
+                    f"""INSERT INTO disruption(response, time_received, api_call_log_id) 
+                    SELECT PARSE_JSON('{msg_json}'), '{timestamp}', {api_call_log_id};"""
                 )
             except Exception as e:
-                print(f'Error when trying to insert data into the db: {e}')
+                print(f'Error when trying to insert disruption data into the db: {e}')
                 conn.close()
                 return
                 
--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from get_api_data import TFLData
 
 if __name__ == '__main__':
     tfl_data = TFLData()
-    api_data, time_received = tfl_data.get_api_data()
-    tfl_data.write_data(api_data, time_received)
+    api_data, time_received, api_call_log_id = tfl_data.get_api_data()
+    tfl_data.write_data(api_data, time_received, api_call_log_id)
